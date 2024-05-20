6c1196d6e8fab8f56106160b845d86ab630364b2
Kooreis <kooreis.official@gmail.com>
2024-05-20 07:32:57 -0500

    This update adds the copyRandomList function which performs a deep copy of a linked list with random pointers. It first creates a mapping of original nodes to their corresponding new nodes. Then it assigns the next and random pointers for the new nodes using the mapping.

--- solution.java
+++ solution.java
@@ -1,14 +1,25 @@
-import java.util.HashMap;
-import java.util.Map;
+public class Main {
+    public static Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
 
-class Node {
-    int val;
-    Node next;
-    Node random;
+        Map<Node, Node> map = new HashMap<>();
+        Node node = head;
 
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
+        while (node != null) {
+            map.put(node, new Node(node.val));
+            node = node.next;
+        }
+
+        node = head;
+
+        while (node != null) {
+            map.get(node).next = map.get(node.next);
+            map.get(node).random = map.get(node.random);
+            node = node.next;
+        }
+
+        return map.get(head);
     }
 }
