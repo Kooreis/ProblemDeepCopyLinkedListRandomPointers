e9453d97dff2fb288f14dc2ec8234363257bb478
Kooreis <kooreis.official@gmail.com>
2024-05-20 07:32:57 -0500

    This update introduces the Solution class, which contains methods to clone a linked list with random pointers. The GetClonedNode method returns a clone of a given node, and the CopyRandomList method returns a deep copy of a linked list.

--- solution.cs
+++ solution.cs
@@ -1,16 +1,40 @@
-using System;
-using System.Collections.Generic;
-
-public class Node
+public class Solution
 {
-    public int val;
-    public Node next;
-    public Node random;
+    Dictionary<Node, Node> visited = new Dictionary<Node, Node>();
 
-    public Node(int _val)
+    public Node GetClonedNode(Node node)
     {
-        val = _val;
-        next = null;
-        random = null;
+        if (node != null)
+        {
+            if (this.visited.ContainsKey(node))
+            {
+                return this.visited[node];
+            }
+            else
+            {
+                this.visited[node] = new Node(node.val);
+                return this.visited[node];
+            }
+        }
+        return null;
+    }
+
+    public Node CopyRandomList(Node head)
+    {
+        if (head == null) return null;
+
+        Node oldNode = head;
+        Node newNode = new Node(oldNode.val);
+        this.visited[oldNode] = newNode;
+
+        while (oldNode != null)
+        {
+            newNode.random = this.GetClonedNode(oldNode.random);
+            newNode.next = this.GetClonedNode(oldNode.next);
+
+            oldNode = oldNode.next;
+            newNode = newNode.next;
+        }
+        return this.visited[head];
     }
 }
