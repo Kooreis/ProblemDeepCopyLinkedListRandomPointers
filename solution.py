efd102168b351d062a7bfd78db285fadf622d439
Kooreis <kooreis.official@gmail.com>
2024-05-20 07:32:57 -0500

    This update adds the copyRandomList function which creates a deep copy of the linked list. It uses a dictionary to store the old nodes as keys and the new nodes as values.

--- solution.py
+++ solution.py
@@ -1,5 +1,16 @@
-class Node:
-    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
-        self.val = int(x)
-        self.next = next
-        self.random = random
+def copyRandomList(head: 'Node') -> 'Node':
+    if not head:
+        return head
+
+    old_node = head
+    new_node = Node(old_node.val, None, None)
+    node_dict = {old_node: new_node}
+
+    while old_node:
+        new_node.random = getClonedNode(old_node.random, node_dict)
+        new_node.next = getClonedNode(old_node.next, node_dict)
+
+        old_node = old_node.next
+        new_node = new_node.next
+
+    return node_dict[head]
